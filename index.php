7500577acfcca2eebb98f0cf32b2b62d8aac1947
hugobarrioser <hugobarrioser@gmail.com>
2020-07-18 15:20:35 -0600

    meta title fixed

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1”>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Metadatos SEO -->
     <title>En Corto Producciones | Audiovisual | Argentina</title>
     <meta name="Description"
